3c278120c812132daa1dc03cabb6d9d60d91de55
OlenaLotos <olenameshanych@gmail.com>
2022-05-11 15:58:52 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,29 @@
 const input = document.querySelector('#validation-input');
+const lengthInput = document.querySelector('[data-length]');
+ 
+
+// console.log(input);
+// // function onInputBlur() {
+// //     const lengthInput = input.value.length;
+// //     if (input.value.length = input.lengthInput) {
+// //         input.style.valid;
+// //     } else {
+// //        input.style.invalid;
+// //     };
+
+// //     console.log(input.value.length);
+// // }
 
 input.addEventListener('blur', onInputBlur);
-
-console.log(input);
-function onInputBlur() { 
-    if (input.value.length = input.data-length) {
-        .valid; 
+function onInputBlur() {
+    
+    if (input.value === lengthInput) {
+        input.style.valid;
     } else {
-        .invalid;
+        input.style.invalid;
+    };
+    console.log(input.value); 
+     console.log(lengthInput); 
 };
 
-не зроблене ще
+
