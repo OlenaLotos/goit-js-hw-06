8b879808c210e041814706007a065da2dde01132
OlenaLotos <olenameshanych@gmail.com>
2022-05-11 11:20:39 +0300

    doing 1 task (full)

--- js/task-01.js
+++ js/task-01.js
@@ -2,23 +2,22 @@
 const itemRef = document.querySelectorAll('.item');
 console.log("Number of categories:" + itemRef.length);
 
-const catRef = document.querySelectorAll('h2');
+itemRef.forEach(item => {
+  console.log("Category: " + item.firstElementChild.textContent);
+console.log("Elements:" + item.lastElementChild.children.length)}
+  
+);
 
-console.log(catRef);
 
-for (const cat of catRef) {
-    console.log("Category:", cat.textContent);
+
+
+// const catRef = document.querySelectorAll('h2');
+
+// console.log(catRef);
+
+// for (const cat of catRef) {
+//     console.log("Category:", cat.textContent);
     
-}
-const elementsRef = document.querySelectorAll('li >h2 > ul');
-console.log(elementsRef);
-for (const el of elementsRef) {
-   
-  console.log("Elements:" + el.length);  
-}
+// }
 
-
-// const itemsRef = item[0].querySelectorAll('li');
-
-// console.log(itemsRef);
 
