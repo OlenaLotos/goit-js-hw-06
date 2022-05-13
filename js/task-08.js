df2eb7e5ca88277ce9a57c37ace6b8ae0d869dd9
OlenaLotos <olenameshanych@gmail.com>
2022-05-13 11:24:19 +0300

    doinf 8 task (almost full)

--- js/task-08.js
+++ js/task-08.js
@@ -1,29 +1,30 @@
 const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', onFormSubmit);
+
 function onFormSubmit(event) {
     event.preventDefault();
     
     const formElements = event.currentTarget.elements;
 
-    console.dir(formElements);
+    // console.dir(formElements);
 
     const mail = formElements.email.value;
     const password = formElements.password.value;
-    console.log(mail, password);
+  
+    if (mail.value === "" || password.value === "") {
+    return alert("Введіть всі дані");
+  };
+    console.log('e-mail:', mail, "password:", password);
+     event.currentTarget.reset();
+};
 
-      // const formData = new FormData(event.currentTarget);
-    // console.log(formData);
-    // formData.forEach((value, name) => {
-    //     console.log(`${name}: ${value}`);
-    // });
-}
 
-//reset form
-//   const createButtonReset = () => {
-//         return
-//         `<button type="reset">Login</button>`;
-//     };
+  
 
-//     form.insertAdjacentHTML('beforeend', createButtonReset());
-// lдодати ресет і алерт
+
+
+
+
+  
+
