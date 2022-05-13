f637a98242a1b8eadcda46740715936b647b1527
OlenaLotos <olenameshanych@gmail.com>
2022-05-13 12:31:25 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,19 +3,19 @@ const form = document.querySelector('.login-form');
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
     
-    const formElements = event.currentTarget.elements;
+  const formElements = event.currentTarget.elements;
 
-    // console.dir(formElements);
+  // console.dir(formElements);
 
-    const mail = formElements.email.value;
-    const password = formElements.password.value;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
   
-    if (mail === "" || password === "") {
+  if (mail === "" || password === "") {
     return alert("Введіть всі дані");
   };
-    console.log('{e-mail:', mail, "password:", password);
+  console.log({ mail, password});
      event.currentTarget.reset();
 };
 
