66468e79ed6a12f8a95ed6bd14d673546441865d
OlenaLotos <olenameshanych@gmail.com>
2022-05-17 16:25:09 +0300

    task 6 new

--- js/task-06.js
+++ js/task-06.js
@@ -4,21 +4,20 @@ const lengthInput = document.querySelector('input[data-length]');
 inputForm.addEventListener('focus', onInputFocus);
 inputForm.addEventListener('blur', onInputBlur);
 
-function onInputFocus() {
-    
+function onInputFocus() { 
 };
 
 function onInputBlur(event) {
     
-    if (event.currentTarget.value.length === (Number(lengthInput.dataset.length))){ 
- inputForm.classList.add('valid');
+    if (event.currentTarget.value.length === (Number(lengthInput.dataset.length))) { 
+       inputForm.classList.add('valid'); inputForm.classList.remove('invalid');
+        
 }
     else {
+        inputForm.classList.remove('valid');
         inputForm.classList.add('invalid');
-        
-        }
+   }
     }
     
-
 
 
